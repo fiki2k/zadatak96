df459ddd989e26e1ea51bfce6a0f8047261eb31f
fiki2k <91737444+fiki2k@users.noreply.github.com>
2022-02-16 10:14:29 +0100

    Add files via upload

---
+++ zad96.c
@@ -0,0 +1,22 @@
+// Program treba naci sve parne i neparne brojeve u polja.
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int polje[10], Parni = 0, Neparni = 0, i;
+    printf("Unesite 10 brojeva: ");
+    for (i = 0; i < 10; i++)
+        scanf("%d", &polje[i]);
+    for (i = 0; i < 10; i++)
+    {
+        if (polje[i] % 2 == 0)
+            Parni++;
+        else
+            Neparni++;
+    }
+    printf("\nUkupan broj parnih brojeva je = %d", Parni);
+    printf("\nUkupan broj neparnih brojeva je = %d", Neparni);
+    return 0;
+} 
